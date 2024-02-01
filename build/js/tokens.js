7316429dc14e36b2b08cb066786cb41ca3cb56f8
Gabriel Rodrigues <gabriel.rocha.andrade.rodrigues@gmail.com>
2024-02-01 09:21:50 -0300

    Initial commit

---
+++ build/js/tokens.js
@@ -0,0 +1,13 @@
+/**
+ * Do not edit directly
+ * Generated on Thu, 01 Feb 2024 12:15:25 GMT
+ */
+
+module.exports = {
+  "ColorWhite": "#ffffff",
+  "ColorGray200": "#c1c1c4",
+  "ColorGray500": "#50505c",
+  "ColorGray800": "#1c1c1f",
+  "ColorGray900": "#121214",
+  "ColorPurple500": "#8257e6"
+};
